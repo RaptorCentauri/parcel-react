3477e9849777071d89e8fe523cdd884051aaa4d7
RaptorCentauri <dave.leo.shilander@icloud.com>
2018-06-28 17:17:00 -0500

    Initial Commit

---
+++ app/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+class App extends React.Component{
+    state = {
+    }
+
+    render(){
+        return(
+            <div className='App'>
+                
+            </div>
+        );
+    }
+}
+
+render(<App/>,window.document.getElementById('root'));
